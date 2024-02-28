f788854302ceadd58a4140ed6e3eda41879e4b9d
FatngatirBilek <fathirbimashabri@gmail.com>
2024-02-28 09:07:32 +0700

    FINAL

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import DrawerButton from '@/components/DrawerButton'
+import Providers from '@/components/Providers'
+import Sidebar from '@/components/Sidebar'
+import Header from '@/components/header/Header'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'KANTIN PM',
+  description: 'E-Kantin',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers>
+          <div className="drawer">
+            <DrawerButton />
+            <div className="drawer-content">
+              <div className="min-h-screen flex flex-col">
+                <Header />
+                {children}
+                <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                  <p>
+                   Made with ❤️ - Fathir
+                  </p>
+                </footer>
+              </div>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <Sidebar />
+            </div>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}

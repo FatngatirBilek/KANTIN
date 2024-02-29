5189eb2cbfae6fa1af7d7ae70d7171b0be122d25
FatngatirBilek <fathirbimashabri@gmail.com>
2024-02-29 13:22:02 +0700

    SpeedTestNext

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
                 <SpeedInsights/>
                 <footer className="footer footer-center p-4 bg-base-300 text-base-content">
                   <p>
-                   Made with ❤️ - Fathir
+                   Made with ❤️ - F
                   </p>
                 </footer>
               </div>
